ff0c141c7d61372e4b9d3021cb809ac4db6396dc
Yohann Candido <canyoh7@gmail.com>
2024-12-20 07:42:59 -0700

    change: open graphql api

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -4,7 +4,7 @@ class GraphqlController < ApplicationController
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
-  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
 
   def execute
     variables = prepare_variables(params[:variables])
@@ -47,6 +47,6 @@ class GraphqlController < ApplicationController
     logger.error e.message
     logger.error e.backtrace.join("\n")
 
-    render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: 500
+    render json: { errors: [ { message: e.message, backtrace: e.backtrace } ], data: {} }, status: 500
   end
 end
