4abaf241e2df6b0d618927ec36b3504d0eda6372
Yohann Candido <canyoh7@gmail.com>
2025-01-02 16:47:13 -0700

    fix: javascript file location

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".see-more").forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const taskDescription = link.closest(".task-description");
+      taskDescription.querySelector(".short-description").classList.toggle("hidden");
+      taskDescription.querySelector(".full-description").classList.toggle("hidden");
+      link.classList.toggle("hidden");
+    });
+  });
+
+  document.querySelectorAll(".show-less").forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const taskDescription = link.closest(".task-description");
+      taskDescription.querySelector(".short-description").classList.toggle("hidden");
+      taskDescription.querySelector(".full-description").classList.toggle("hidden");
+      taskDescription.querySelector(".see-more").classList.toggle("hidden");
+    });
+  });
+});
