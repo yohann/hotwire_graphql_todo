b9c85e5f990ef0df55c32816d8b069da47b833d1
Yohann Candido <canyoh7@gmail.com>
2025-01-02 16:21:31 -0700

    change: displaying toggleable description

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,24 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".see-more").forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      const taskDescription = link.closest(".task-description");
+      taskDescription.querySelector(".short-description").classList.toggle("hidden");
+      taskDescription.querySelector(".full-description").classList.toggle("hidden");
+      link.classList.toggle("hidden");
+    });
+  });
+
+  document.querySelectorAll(".show-less").forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const taskDescription = link.closest(".task-description");
+      taskDescription.querySelector(".short-description").classList.toggle("hidden");
+      taskDescription.querySelector(".full-description").classList.toggle("hidden");
+      taskDescription.querySelector(".see-more").classList.toggle("hidden");
+    });
+  });
+});
